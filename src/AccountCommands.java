2f61f0ced3171f2dd2849457e9e2907bc4fb8a63
Charlie Reilly <cfr8511@rit.edu>
2021-10-24 16:18:49 -0400

    help method to include some of the commands that we will have

--- src/AccountCommands.java
+++ src/AccountCommands.java
@@ -2,8 +2,35 @@ import java.sql.*;
 
 public class AccountCommands {
 
+    /**
+     * Displays to the user what commands do what
+     */
+    public static void help(){
+        System.out.println("" +
+                "!help ..." +
+                "!follow [email] ..." +
+                "!unfollow [email] ... " +
+                "!create_collection [name] ..." +
+                "!add_to_collection [name] [song/album] ..." +
+                "!delete_collection [name] ..." +
+                "!change_collection_name [old_name] [new_name] ..." +
+                "" +
+                "" +
+                "" +
+                "" );
 
+    }
+
+
+    /**
+     * Handles the follow command
+     * @param conn - the database connection
+     * @param local_name - the name of the follower; person inputting command
+     * @param other_email - the email of the person to be followed
+     * @throws SQLException
+     */
     public static void follow(Connection conn, String local_name, String other_email) throws SQLException {
+        //TODO: implement the call in PostgresSSHTest
         Statement stment = conn.createStatement();
         ResultSet rset = stment.executeQuery("" +
                 "select email from User where email = 'other_email'");
@@ -20,5 +47,9 @@ public class AccountCommands {
 
 
         stment.close();
+    }
+
+    public static void unfollow(Connection conn, String local_name, String other_email){
+
     }
 }
