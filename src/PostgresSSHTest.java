5e28a152358fbe11a715b52a7f8c3d2b1a65a0a5
Zach Machtinger <zbm8121@rit.edu>
2021-10-26 19:42:46 -0400

    finished inputting album data, still need to input album_songs and album_genre

--- src/PostgresSSHTest.java
+++ src/PostgresSSHTest.java
@@ -3,9 +3,9 @@ import com.jcraft.jsch.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.*;
-import java.util.Arrays;
-import java.util.Properties;
-import java.util.Scanner;
+import java.sql.Date;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class PostgresSSHTest {
 
@@ -45,8 +45,80 @@ public class PostgresSSHTest {
             conn = DriverManager.getConnection(url, props);
             System.out.println("Database connection established");
 
+            Scanner scanner = new Scanner(new File("SampleData/artists-songs-albums-tags.csv"));
+            HashMap<String, Date> albums = new HashMap<>();
+            PreparedStatement pStmt = conn.prepareStatement("select release_date from song where title = ?");
+
+            while (scanner.hasNextLine()) {
+                String[] row = scanner.nextLine().split(",");
+                pStmt.setString(1, row[0].toLowerCase());
+                ResultSet rSet = pStmt.executeQuery();
+                rSet.next();
+                Date newDate = rSet.getDate(1);
+                if (albums.containsKey(row[2])) {
+                    if (albums.get(row[2]).compareTo(newDate) < 0) {
+                        albums.put(row[2], newDate);
+                    }
+                } else {
+                    albums.put(row[2], newDate);
+                }
+            }
+
+            Object[][] albumData = new Object[albums.size()][3];
+
+            int i = 0;
+            for (String s : albums.keySet()) {
+                albumData[i][1] = s;
+                albumData[i][2] = randomDateBetween(albums.get(s));
+                i++;
+            }
+
+            HashMap<String, HashSet<String>> artists = new HashMap<>();
+
+            Scanner scan2 = new Scanner(new File("SampleData/artists-songs-albums-tags.csv"));
+            while (scan2.hasNextLine()) {
+                String[] row = scan2.nextLine().split(",");
+                if (!artists.containsKey(row[2])) {
+                    artists.put(row[2], new HashSet<>());
+                }
+                artists.get(row[2]).add(row[1]);
+            }
+
+            i = 0;
+            for (String s : artists.keySet()) {
+                String[] albumArtists = artists.get(s).toArray(new String[0]);
+                String artist = "";
+                for (int j = 0; j < albumArtists.length; j++) {
+                    if (j != albumArtists.length - 1) {
+                        artist += albumArtists[j] + ", ";
+                    } else {
+                        artist += albumArtists[j];
+                    }
+                }
+                albumData[i][0] = artist;
+                i++;
+            }
+
+            /*for (int j = 0; j < albumData.length; j++) {
+                System.out.println("The Album (" + albumData[j][1] + ") released on: " + albumData[j][2]);
+                System.out.println("   Was Made By: " + albumData[j][0]);
+            }*/
+
+            PreparedStatement pStmt2 = conn.prepareStatement("insert into album values(?, ?, ?, ?)");
+            for (int j = 0; j < albumData.length; j++) {
+                try {
+                    pStmt2.setInt(1, j + 1);
+                    pStmt2.setString(2, (String) albumData[j][0]);
+                    pStmt2.setString(3, (String) albumData[j][1]);
+                    pStmt2.setDate(4, (Date) albumData[j][2]);
+                    pStmt2.executeUpdate();
+                } catch (SQLException sqle) {
+                    System.out.println("Couldn't add tuple: " + sqle);
+                }
+            }
+
             // Putting in all song info
-            /*PreparedStatement pStmt = conn.prepareStatement("insert into song values(?,?,?,?,?,?)");
+            /* PreparedStatement pStmt = conn.prepareStatement("insert into song values(?,?,?,?,?,?)");
             Scanner scanner = new Scanner(new File("SampleData/artists-songs-albums-tags.csv"));
             int i = 1;
             while (scanner.hasNextLine()) {
@@ -103,5 +175,14 @@ public class PostgresSSHTest {
         int year = randomInRange(1980, 2020);
         String dateString = String.format("%d-%02d-%02d", year, month, day);
         return Date.valueOf(dateString);
+    }
+
+    private static Date randomDateBetween(Date startDate) {
+        long startMillis = startDate.getTime();
+        Date endDate = Date.valueOf("2021-01-01");
+        long endMillis = endDate.getTime();
+        long randomMillis = ThreadLocalRandom.current().nextLong(startMillis, endMillis);
+
+        return new Date(randomMillis);
     }
 }
