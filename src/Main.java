3c6f88639d7469cbe638d9a546756d9d42138fc6
Zach Machtinger <zbm8121@rit.edu>
2021-10-27 16:10:27 -0400

    beginning to implement playing songs

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
     }
 }
