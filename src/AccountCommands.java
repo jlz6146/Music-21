b1589603f781071844ad3a6e2dc9a741f2ae7004
Zach Machtinger <zbm8121@rit.edu>
2021-10-27 16:34:01 -0400

    beginning to integrate with database

--- src/AccountCommands.java
+++ src/AccountCommands.java
@@ -1,7 +1,19 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class AccountCommands {
-    public static void play_song(Connection conn, String title) {
+    public static void play_song(Connection conn, String title) throws SQLException {
+        try {
+            PreparedStatement pStmt = conn.prepareStatement("select album_id from album where album_name = ?");
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("Closing Database Connection");
+                conn.close();
+            }
+        }
     }
 }
