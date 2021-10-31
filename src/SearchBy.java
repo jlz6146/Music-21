c7c796f8410b48a47fc40475866cef8e654276b5
Jesse Zhao <jlz6146@g.rit,edu>
2021-10-31 14:53:23 -0400

    exit working, initial search order working

--- src/SearchBy.java
+++ src/SearchBy.java
@@ -11,11 +11,11 @@ import java.time.LocalDate;
 public class SearchBy {
 
     public enum OrderType {
-        title ("S.title"),
-        artist ("S.artist_name"),
-        album ("A.album_name"),
-        genre ("S.genre_name"),
-        year ("S.release_date");
+        title ("song.title"),
+        artist ("song.artist_name"),
+        album ("album.album_name"),
+        genre ("song.genre_name"),
+        year ("song.release_date");
 
         public final String label;
 
@@ -30,24 +30,22 @@ public class SearchBy {
     public static void Search(Connection conn) throws SQLException {
 
 
-        Statement stmt = conn.createStatement();
+            Boolean searchRunning = true;
+
+            Statement stmt = conn.createStatement();
 
             System.out.println("Input search keyword (title, artist, album, genre):");
             String input = search.nextLine();
 
-            /*String sQuery =
-                    "Select S.song_id, S.title, S.artist_name, A.album_name, S.length, S.release_date, U_P.plays" +
-                            " From song as S, album_songs as A_S, album as A," +
-                            " (Select song_id, sum(user_play_count) as plays From user_play Group By song_id) as U_P" +
-                            " Where S.song_id = A_S.song_id AND A_S.album_id = A.album_id AND" +
-                            " (S.title like '%" + input.toLowerCase() + "%' OR S.artist_name like '%" + input.toUpperCase() + "%' OR S.genre_name like '%" + input.toLowerCase() + "%'OR A.album_name LIKE '%" + input.toUpperCase() + "')";
-*/
-            PreparedStatement sQuery = conn.prepareStatement("select song.song_id, song.title, song.artist_name, album.album_name, song.length, song.release_date, coalesce(u_p.plays, 0) as plays " +
-                        "from song " +
-                        "inner join album_songs on album_songs.song_id = song.song_id " +
-                        "inner join album on album_songs.album_id = album.album_id " +
-                        "left join (select song_id, sum(user_play_count) as plays from user_play group by song_id) as u_p on u_p.song_id = song.song_id " +
-                        "Where (song.title like ? or song.artist_name like ? or song.genre_name like ? or album.album_name like ?) ");
+            String searchBy = "select song.song_id, song.title, song.artist_name, album.album_name, song.length, song.release_date, coalesce(u_p.plays, 0) as plays " +
+                    "from song " +
+                    "inner join album_songs on album_songs.song_id = song.song_id " +
+                    "inner join album on album_songs.album_id = album.album_id " +
+                    "left join (select song_id, sum(user_play_count) as plays from user_play group by song_id) as u_p on u_p.song_id = song.song_id " +
+                    "Where (song.title like ? or song.artist_name like ? or song.genre_name like ? or album.album_name like ?) ";
+            String orderBy = "Order by song.title, song.artist_name asc";
+
+            PreparedStatement sQuery = conn.prepareStatement(searchBy + orderBy);
 
             sQuery.setString(1, "%" + input.toLowerCase() + "%");
             sQuery.setString(2, "%" + input.toUpperCase() + "%");
@@ -59,14 +57,14 @@ public class SearchBy {
                 System.out.println(rs.getString("title") + " " + rs.getString("artist_name") + " " + rs.getString("album_name") + " " + rs.getTime("length") + " " + rs.getDate("release_date") + " " + rs.getInt("plays"));
             }
 
-            while(true) {
+            while(searchRunning) {
                 System.out.println("reorder (reorder)     ||      search again (search)    ||      exit search (exit)");
                 String inNext = search.nextLine().replaceAll("[^A-Za-z]+ ", "").toLowerCase();
                 if (inNext.equals("search")) {
                     Search(conn);
                 }
                 else if (inNext.equals("exit")) {
-                    break;
+                    searchRunning = false;
                 }
                 else if (!inNext.equals("reorder") && !inNext.equals("search") && !inNext.equals("exit")) {
                     System.out.println("Sorry, please input a valid argument.");
@@ -77,9 +75,9 @@ public class SearchBy {
 
                     while (true) {
                         System.out.println("reorder by: title | artist | album | genre | year");
-                        String orderBy = search.nextLine();
+                        String orderByTerm = search.nextLine().replaceAll("[^A-Za-z]+ ", "").toLowerCase();
                         try {
-                            type = OrderType.valueOf(orderBy);
+                            type = OrderType.valueOf(orderByTerm);
                             break;
                         } catch (IllegalArgumentException E) {
                             System.out.println("Invalid order type, please try again");
@@ -89,7 +87,7 @@ public class SearchBy {
 
                     while (true) {
                         System.out.println("ascending/descending? (asc/desc)");
-                        String ascDesc = search.nextLine();
+                        String ascDesc = search.nextLine().replaceAll("[^A-Za-z]+ ", "").toLowerCase();
                         try {
                             dir = OrderDirection.valueOf(ascDesc);
                             break;
@@ -99,7 +97,7 @@ public class SearchBy {
                         }
                     }
 
-                    PreparedStatement orderQuery = conn.prepareStatement(sQuery + " Order By " + type + " " + dir);
+                    PreparedStatement orderQuery = conn.prepareStatement(searchBy + " Order By " + type.label + " " + dir);
                     rs = orderQuery.executeQuery();
                     while (rs.next()) {
                         System.out.println(rs.getString("title") + " | " + rs.getString("artist_name") + " | " + rs.getString("album_name") + " | " + rs.getTime("length") + " | " + rs.getDate("release_date") + " | " + rs.getInt("plays"));
